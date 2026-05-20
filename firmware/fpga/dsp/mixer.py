dc948cd1cb89b880ac70a8dbb681d5a5f04fd2ed
mndza <diego.hdmp@gmail.com>
2026-05-20 16:28:38 +0200

    gateware: use 3 real multipliers for complex mixer instead of 4

--- firmware/fpga/dsp/mixer.py
+++ firmware/fpga/dsp/mixer.py
@@ -4,14 +4,12 @@
 # Copyright (c) 2025 Great Scott Gadgets <info@greatscottgadgets.com>
 # SPDX-License-Identifier: BSD-3-Clause
 
-from amaranth               import Module
+from amaranth               import Module, Signal
 from amaranth.lib           import wiring, stream
 from amaranth.lib.wiring    import In, Out
 
 from dsp.fir_mac16          import iCE40Multiplier
 from dsp.round              import convergent_round
-
-from util                   import IQSample
 
 
 class ComplexMultiplier(wiring.Component):
@@ -39,42 +37,52 @@ class ComplexMultiplier(wiring.Component):
 
         o_shape = (A * C).shape()
         o_width = o_shape.width
+        o_shape2 = ((A+B)*(C+D)).shape()
+        o_width2 = o_shape2.width
 
         m.submodules.dsp1 = dsp1 = iCE40Multiplier(a_width=a_width, b_width=c_width, p_width=0, o_width=o_width, always_ready=self.always_ready)
-        m.submodules.dsp2 = dsp2 = iCE40Multiplier(a_width=a_width, b_width=d_width, p_width=0, o_width=o_width, always_ready=self.always_ready)
-        m.submodules.dsp3 = dsp3 = iCE40Multiplier(a_width=b_width, b_width=d_width, p_width=0, o_width=o_width, always_ready=self.always_ready)
-        m.submodules.dsp4 = dsp4 = iCE40Multiplier(a_width=b_width, b_width=c_width, p_width=0, o_width=o_width, always_ready=self.always_ready)
+        m.submodules.dsp2 = dsp2 = iCE40Multiplier(a_width=b_width, b_width=d_width, p_width=0, o_width=o_width, always_ready=self.always_ready)
+        m.submodules.dsp3 = dsp3 = iCE40Multiplier(a_width=a_width+1, b_width=d_width+1, p_width=0, o_width=o_width2, always_ready=self.always_ready)
 
         valid_in = self.a.valid & self.b.valid
         m.d.comb += [
             dsp1.a.eq(A),
             dsp1.b.eq(C),
             dsp1.valid_in.eq(valid_in),
-            dsp2.a.eq(A),
+            dsp2.a.eq(B),
             dsp2.b.eq(D),
             dsp2.valid_in.eq(valid_in),
-            dsp3.a.eq(B),
-            dsp3.b.eq(D),
+            dsp3.a.eq(A+B),
+            dsp3.b.eq(C+D),
             dsp3.valid_in.eq(valid_in),
-            dsp4.a.eq(B),
-            dsp4.b.eq(C),
-            dsp4.valid_in.eq(valid_in),
         ]
 
         ready_in = dsp1.ready_in
         if not self.always_ready:
-            m.d.comb += self.a.ready.eq(ready_in)
-            m.d.comb += self.b.ready.eq(ready_in)
+            m.d.comb += self.a.ready.eq(ready_in & self.b.valid)
+            m.d.comb += self.b.ready.eq(ready_in & self.a.valid)
 
-        with m.If(~self.c.valid | self.c.ready):
-            m.d.sync += self.c.valid.eq(dsp1.valid_out)
-            with m.If(dsp1.valid_out):
-                m.d.sync += self.c.p.i.eq(convergent_round(dsp1.o - dsp3.o, o_width - self.c.p.i.shape().width - 1))
-                m.d.sync += self.c.p.q.eq(convergent_round(dsp2.o + dsp4.o, o_width - self.c.p.q.shape().width - 1))
+        mid_valid = Signal()
+        mid_ready = Signal()
+        mid_sum0 = Signal(o_shape)
+        mid_sum1 = Signal(o_shape)
+        mid_dsp2 = Signal(o_shape)
+        with m.If(~mid_valid | mid_ready):
+            m.d.sync += mid_valid.eq(dsp1.valid_out)
+            with m.If(mid_valid):
+                m.d.sync += mid_sum0.eq(dsp1.o - dsp2.o)
+                m.d.sync += mid_sum1.eq(dsp3.o - dsp1.o)
+                m.d.sync += mid_dsp2.eq(dsp2.o)
             m.d.comb += dsp1.ready_out.eq(1)
             m.d.comb += dsp2.ready_out.eq(1)
             m.d.comb += dsp3.ready_out.eq(1)
-            m.d.comb += dsp4.ready_out.eq(1)
+
+        with m.If(~self.c.valid | self.c.ready):
+            m.d.sync += self.c.valid.eq(mid_valid)
+            with m.If(mid_valid):
+                m.d.sync += self.c.p.i.eq(convergent_round(mid_sum0, o_width - self.c.p.i.shape().width - 1))
+                m.d.sync += self.c.p.q.eq(convergent_round(mid_sum1 - mid_dsp2, o_width - self.c.p.q.shape().width - 1))
+            m.d.comb += mid_ready.eq(1)
         
         return m
 
